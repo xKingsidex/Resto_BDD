2aac46decd705ce004b6508e3578d5fa7dc53ee7
ZoZ <enzo.foulon53@gmail.com>
2025-04-20 12:25:34 +0200

    new update debug

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ $dbname = 'resto';
 $username = 'root';
 $password = '';
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
